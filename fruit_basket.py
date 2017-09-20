456aa85f25ab4331c51c82bc2f5a16af9ec8de35
aaronjglynn <aglynn@evolveacademy.io>
2017-09-19 22:55:05 -0400

    with concise functions

--- fruit_basket.py
+++ fruit_basket.py
@@ -1,14 +1,17 @@
+def guess_fruit(basket):
+    guess = raw_input("What's your guess?\n")
+    return (guess in basket)
+
 def main():
     fruit_basket = ["apple","banana","pear","blueberry","melon"]
-    guess = raw_input("What's your guess?\n")
-    guess_num = 1 
-    while guess_num <= 5:
-        if guess in fruit_basket:
+    guess_num = 0
+    while guess_num < 5:
+        result = guess_fruit(fruit_basket)
+        if result:
             print("Nice going!")
             return
-        elif guess not in fruit_basket and guess_num < 5:
+        elif not result:
             print("Incorrect")
-            guess = raw_input("What's your guess?\n")
             guess_num += 1
         else:
             print("You're all outta time punk.")
