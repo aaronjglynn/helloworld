c002fbf8b43977f938b8dc8deade0b004f0ccfe1
aaronjglynn <aglynn@evolveacademy.io>
2017-09-19 22:41:52 -0400

    This version works with the guess counter up to five

--- fruit_basket.py
+++ fruit_basket.py
@@ -1,10 +1,16 @@
 def main():
     fruit_basket = ["apple","banana","pear","blueberry","melon"]
     guess = raw_input("What's your guess?\n")
-    while str(guess) not in fruit_basket:
-        print("you're not correct!")
-        guess = raw_input("What's your next guess?\n")
-    else:
-        print("great job charlie!")
-
+    guess_num = 1 
+    while guess_num <= 5:
+        if guess in fruit_basket:
+            print("Nice going!")
+            return
+        elif guess not in fruit_basket and guess_num < 5:
+            print("Incorrect")
+            guess = raw_input("What's your guess?\n")
+            guess_num += 1
+        else:
+            print("You're all outta time punk.")
+            return
 main()
